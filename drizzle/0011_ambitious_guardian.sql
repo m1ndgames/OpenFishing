2102b4698efef87e42c5be0386daba6ae0de71f2
m1nd <m1nd@m1nd.io>
2026-04-23 23:58:05 +0200

    fix: Provide missing migrations

---
+++ drizzle/0011_ambitious_guardian.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `fish_catch` ADD `share_token` text;--> statement-breakpoint
+ALTER TABLE `lure` ADD `share_token` text;--> statement-breakpoint
+ALTER TABLE `spot` ADD `share_token` text;
